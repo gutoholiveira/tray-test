5a08884722f34e2c9c297e168368fdfd9b8a3ce6
Gustavo Oliveira <gutoholiveira@hotmail.com>
2024-11-02 14:14:10 -0300

    feat(controller): created seller controller with actions

---
+++ app/Http/Controllers/SellerController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Seller\StoreRequest;
+use App\Http\Requests\Seller\UpdateRequest;
+use App\Models\Seller;
+use App\Services\SellerService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class SellerController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(private SellerService $seller_service) {}
+
+    /**
+     * Display a listing of the resource
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        $sellers = $this->seller_service->getAll();
+
+        return response()->json(['message' => 'success', 'data' => ['sellers' => $sellers]], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage
+     *
+     * @param StoreRequest $request
+     * @return JsonResponse
+     */
+    public function store(StoreRequest $request): JsonResponse
+    {
+        $seller = $this->seller_service->store($request);
+
+        return response()->json(['message' => 'success', 'data' => ['seller' => $seller]], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource
+     *
+     * @param Seller $seller
+     * @return JsonResponse
+     */
+    public function show(Seller $seller): JsonResponse
+    {
+        return response()->json(['message' => 'success', 'data' => ['seller' => $seller]], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage
+     *
+     * @param UpdateRequest $request
+     * @param Seller $seller
+     * @return JsonResponse
+     */
+    public function update(UpdateRequest $request, Seller $seller): JsonResponse
+    {
+        $updated_seller = $this->seller_service->update($seller, $request);
+
+        return response()->json(['message' => 'success', 'data' => ['seller' => $updated_seller]], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage
+     *
+     * @param Seller $seller
+     * @return JsonResponse
+     */
+    public function destroy(Seller $seller): JsonResponse
+    {
+        $this->seller_service->delete($seller);
+
+        return response()->json(['message' => 'success', 'data' => []], Response::HTTP_OK);
+    }
+
+    /**
+     * Send Sales report by email
+     *
+     * @param Seller $seller
+     * @return void
+     */
+    public function sendReport(Seller $seller): void
+    {
+        $this->seller_service->sendMail($seller);
+    }
+}
