fe4ace7eecc6ce44750aa4f46e8f89b094373047
Gustavo Oliveira <gutoholiveira@hotmail.com>
2024-11-02 13:23:59 -0300

    feat(db_factory): created seller factory

---
+++ database/factories/SellerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Seller;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vendedor>
+ */
+class SellerFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            Seller::NAME  => fake()->name(),
+            Seller::EMAIL => fake()->unique()->safeEmail(),
+        ];
+    }
+}
