3955e7eaedcec1cd99a00941234310309a8fd7b3
Gustavo Oliveira <gutoholiveira@hotmail.com>
2024-11-02 13:18:54 -0300

    feat(route): created web routes to access the seller and sales menus and render the views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/sellers', function () {
+    return Inertia::render('Sellers/Index');
+})->middleware(['auth', 'verified'])->name('sellers');
+
+Route::get('/sales', function () {
+    return Inertia::render('Sales/Index');
+})->middleware(['auth', 'verified'])->name('sales');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
