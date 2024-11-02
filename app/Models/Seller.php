f28930b5dcbfadda308dc0bf96ddd11a2fe3c8d7
Gustavo Oliveira <gutoholiveira@hotmail.com>
2024-11-02 13:08:04 -0300

    feat(model): created Seller model file

---
+++ app/Models/Seller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Seller extends Model
+{
+    use HasFactory;
+
+    const ID    = 'id';
+    const NAME  = 'name';
+    const EMAIL = 'email';
+
+    /**
+     * The table's name.
+     *
+     * @var string
+     */
+    protected $table = 'sellers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        self::NAME,
+        self::EMAIL,
+    ];
+
+    /**
+     * Get the Seller sales.
+     */
+    public function vendas(): HasMany
+    {
+        return $this->hasMany(Sale::class);
+    }
+}
